f8a2b25d5278f3dcda6246ea5d93af9a0c984bef
Makoto Nishimura <mac.nishimura@gmail.com>
2014-06-09 23:29:59 +0900

    first commit

---
+++ MvvmCrossPageNaviSample/MvvmCrossPageNaviSample.Core/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Cirrious.MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MvvmCrossPageNaviSample.Core.ViewModels
+{
+    class MainPageViewModel : MvxViewModel
+    {
+        private MvxCommand _pageNavigate;
+
+        public ICommand pageNavigate
+        {
+            get
+            {
+                _pageNavigate = new MvxCommand(_navigate);
+                return _pageNavigate;
+            }
+        }
+
+        private void _navigate()
+        {
+            ShowViewModel<FirstViewModel>();
+        }
+    }
+}
